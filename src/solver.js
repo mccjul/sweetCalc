5e6523014cce100f7bd64276aecb023226d0e484
Dethbug <mccjul@gmail.com>
2016-11-03 18:33:56 -0400

    fixed paren stuff

--- src/solver.js
+++ src/solver.js
@@ -5,24 +5,19 @@ export default function solver(string) {
 
 //Pedmas factory: parens -> paren multipication -> exponents -> division -> multiply -> add -> subtract
 function pedmas(array){
-    console.log(array);
     //If we have several parens inside first block find the cIosesed begining paren to end
     //Recursively do the math for parens
     if(array.includes('(') || array.includes(')')){
-
         while(array.indexOf('(') != -1){
             let offset = array.indexOf('(');
             let end = array.indexOf(')');
-            console.log(array.indexOf('(', offset + 1) != -1 && end > array.indexOf('(', offset + 1));
             while(array.indexOf('(', offset + 1) != -1 && end > array.indexOf('(', offset + 1)){
                 offset = array.indexOf('(', offset + 1);
-                console.log('here');
                 if(end == -1 || offset == -1){
                     throw 'NAN';
                 }
             }
             chunk = end + 1 - offset;
-            console.log('here', chunk);
             array.splice(offset, chunk, pedmas(array.slice(offset + 1, end)));
         }
     }
@@ -45,8 +40,8 @@ function pedmas(array){
         }
     });
 
-    //missmatch of parens
-    if(array.length != 1){
+    //weirdness happend
+    if(array.length != 1 || isNaN(array[0])){
         throw 'NaN'
     }
     return array[0];

bb9cc928e208f941b2b79d8b3e101dd34742975f
Dethbug <mccjul@gmail.com>
2016-11-03 17:56:42 -0400

    made the entry easier by removing restrictions and refactored solver

--- src/solver.js
+++ src/solver.js
@@ -1,110 +1,80 @@
-//Julian McCarthy Slover
+//Julian McCarthy Solver
 export default function solver(string) {
-    var array = toOperationsArray(string);
-    return pedmas(array);
+    return pedmas(string.split(' ').filter((element)=> element != ''));
 };
-
-//Turn string into array symobol and numbers
-function toOperationsArray(string) {
-    var numArray = [];
-    var offset = 0;
-    for (var char = 0; char < string.length; char++) {
-        if(string.charAt(char) == '*' || string.charAt(char) == '/' || string.charAt(char) == '-' || string.charAt(char) == '+' || string.charAt(char) == '^'){
-            if(string.charAt(char-1) != ')'){
-                numArray.push(parseFloat(string.substring(offset, char)));
-            }
-            numArray.push(string.charAt(char));
-            offset = char + 1;
-        } else if(string.charAt(char) == '('){
-            if(char != 0 && string.charAt(char - 1) != '*' && string.charAt(char - 1) != '/' && string.charAt(char - 1) != '-' && string.charAt(char - 1) != '+' && string.charAt(char - 1) != '^' && string.charAt(char - 1) != '(' && string.charAt(char - 1) != ')'){
-                numArray.push(parseFloat(string.substring(offset, char)));
-            }
-            numArray.push(string.charAt(char));
-            offset = char + 1;
-        } else if(string.charAt(char) == ')'){
-            if(string.charAt(char-1) != ')')
-                numArray.push(parseFloat(string.substring(offset, char)));
-            numArray.push(string.charAt(char));
-            offset = char;
-        } else if(char == string.length - 1){
-            numArray.push(parseFloat(string.substring(offset , char + 1)));
-        }
-    }
-    return numArray;
-}
 
 //Pedmas factory: parens -> paren multipication -> exponents -> division -> multiply -> add -> subtract
 function pedmas(array){
+    console.log(array);
+    //If we have several parens inside first block find the cIosesed begining paren to end
+    //Recursively do the math for parens
     if(array.includes('(') || array.includes(')')){
+
         while(array.indexOf('(') != -1){
-            var index = array.indexOf('(');
-            var end = array.indexOf(')');
-            var offset = index;
+            let offset = array.indexOf('(');
+            let end = array.indexOf(')');
+            console.log(array.indexOf('(', offset + 1) != -1 && end > array.indexOf('(', offset + 1));
             while(array.indexOf('(', offset + 1) != -1 && end > array.indexOf('(', offset + 1)){
                 offset = array.indexOf('(', offset + 1);
+                console.log('here');
                 if(end == -1 || offset == -1){
                     throw 'NAN';
                 }
             }
-            var chunk = end - offset + 1;
+            chunk = end + 1 - offset;
+            console.log('here', chunk);
             array.splice(offset, chunk, pedmas(array.slice(offset + 1, end)));
         }
     }
-    if(hasParenMultipication(array) != -1){
-        while(hasParenMultipication(array) != -1){
-            var index = hasParenMultipication(array);
-            array.splice(index, 2, array[index] * array[index+1]);
+
+    //If no operator is seperating the numbers
+    if(indexOfParenMultipication(array) != -1){
+        while(indexOfParenMultipication(array) != -1){
+            let index = indexOfParenMultipication(array);
+            array.splice(index, 2, parseFloat(array[index]) * parseFloat(array[index+1]));
         }
     }
-    if(array.includes('^')){
-        while(array.indexOf('^') != -1) {
-            var index = array.indexOf('^');
-            array.splice(index-1, 3, Math.pow(array[index-1], array[index+1]));
+
+    //All these operators have the same patterns
+    ['^', '/', '*', '+', '-'].forEach((element) => {
+        if(array.includes(element)){
+            while(array.indexOf(element) != -1) {
+                let index = array.indexOf(element);
+                array.splice(index-1, 3, stringToOperator(array, index, element));
+            }
         }
-    }
-    if(array.includes('/')){
-        while(array.indexOf('/', offset) != -1) {
-            var index = array.indexOf('/');
-            array.splice(index-1, 3, array[index-1] / array[index+1]);
-        }
-    }
-    if(array.includes('*')){
-        while(array.indexOf('*') != -1) {
-            var index = array.indexOf('*');
-            array.splice(index-1, 3, array[index-1] * array[index+1]);
-        }
-    }
-    if(array.includes('+')){
-        var offset = 0;
-        while(array.indexOf('+') != -1) {
-            var index = array.indexOf('+');
-            array.splice(index-1, 3, array[index-1] + array[index+1]);
-        }
-    }
-    if(array.includes('-')){
-        var offset = 0;
-        while(array.indexOf('-') != -1) {
-            var index = array.indexOf('-');
-            array.splice(index-1, 3, array[index-1] - array[index+1]);
-        }
-    }
+    });
+
+    //missmatch of parens
     if(array.length != 1){
-        //missmatch of parenthisis
         throw 'NaN'
     }
     return array[0];
 }
 
-function hasParenMultipication(arr){
-    var array = arr;
+function stringToOperator(array, index, operator){
+    let firstNumber = parseFloat(array[index - 1]);
+    let secondNumber = parseFloat(array[index + 1]);
+    if(operator == '^')
+        return Math.pow(firstNumber, secondNumber);
+    if(operator == '/')
+        return firstNumber / secondNumber;
+    if(operator == '*')
+        return firstNumber * secondNumber;
+    if(operator == '+')
+        return firstNumber + secondNumber;
+    if(operator == '-')
+        return firstNumber - secondNumber;
+}
+
+function indexOfParenMultipication(array){
     if(array.length == 1)
         return -1;
-    for (var i = 0; i < array.length - 1; i++) {
-        var isNumber = array[i] != '*' && array[i] != '/' && array[i] != '+' && array[i] != '-' && array[i] != '^';
-        var nextIsNumber = array[i + 1] != '*' && array[i + 1] != '/' && array[i + 1] != '+' && array[i + 1] != '^';
-        if(isNumber && nextIsNumber){
+    for (let i = 0; i < array.length - 1; i++) {
+        let isNumber = array[i] != '*' && array[i] != '/' && array[i] != '+' && array[i] != '-' && array[i] != '^' && array[i] != '(' && array[i] != ')';
+        let nextIsNumber = array[i + 1] != '*' && array[i + 1] != '/' && array[i + 1] != '+' && array[i + 1] != '^' && array[i + 1] != '(' && array[i + 1] != ')';
+        if(isNumber && nextIsNumber)
             return i;
-        }
     }
 	return -1;
 }
